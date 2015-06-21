b732ba999d61492588d889cb09fb71e310828d48
XAVI <xxargay@yahoo.com>
2015-06-21 12:04:29 +0200

    actualització arxius

--- js/main.js
+++ js/main.js
@@ -1,18 +1,3 @@
    $(document).ready(function() {
-       $.ajax({
-           url: "menu.html",
-           success: function(result) {
-               $("#page").prepend(result);
-
-               $("#menu").mmenu({
-                   "header": {
-                       "title": "PhoneGap Demo Menu",
-                       "add": true,
-                       "update": true
-                   }
-               });
-           }
-       });
-
        FastClick.attach(document.body);
    });
